ba67da94e68e7794f3a5c5d16939cb7974b89f1a
Jerry Chien <chiungkunchien@gmail.com>
2023-12-03 00:29:16 +0000

    complete project

--- main.go
+++ main.go
@@ -64,6 +64,24 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// set content type
+	w.Header().Set("Content-Type", "application/json")
+	// Params
+	params := mux.Vars(r)
+	// loop over the movies, range
+	// delete the movie with the id that you've sent
+	// add a new movie - the movie that we send  in the body of position
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 
 }
 
